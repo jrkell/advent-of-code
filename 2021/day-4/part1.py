1b39940d43a1856dbcd7a8ba92999189a41c8521
Jared Kelly <jared.kelly@dominos.com.au>
2021-12-06 08:17:51 +1000

    more

--- 2021/day-4/part1.py
+++ 2021/day-4/part1.py
@@ -1,3 +1,10 @@
 def openInput():
     with open('input.txt') as f:
         return f.read().splitlines()
+
+class Card():
+    def __init__(self) -> None:
+        self.grid = [['' for j in range(0,5)] for i in range(0,5)]
+
+    def setCell(self, x, y, value):
+        self.grid[y][x] = value
