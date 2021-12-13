d5022c0877c806f04b0b20cf455fe43fc90a393f
Jared Kelly <jarredkelly@gmail.com>
2021-12-13 20:19:31 +1000

    day 10 gg ez

--- 2021/day-10/part2.py
+++ 2021/day-10/part2.py
@@ -1,3 +1,4 @@
+#%% 
 import os
 
 OPENERS = ['{','(','[','<']
@@ -6,22 +7,22 @@ BRACKETS = [
     {
         "open": "(",
         "close": ")",
-        "points": 3
+        "points": 1
     },
     {
         "open": "[",
         "close": "]",
-        "points": 57
+        "points": 2
     },
     {
         "open": "{",
         "close": "}",
-        "points": 1197
+        "points": 3
     },
     {
         "open": "<",
         "close": ">",
-        "points": 25137
+        "points": 4
     },
 ]
 
@@ -53,19 +54,32 @@ def containsCloser(string: str):
 def getClosingBracket(value: str) -> str:
     return next(bracket["close"] for bracket in BRACKETS if bracket["open"] == value)
 
-def getCorruptPoints(string: str):
+def getIncompletePoints(string: str):
     adj_string = removePairs(string)
     closer = containsCloser(adj_string)
-    if closer != None:
-        return getPoints(closer)
+    if closer == None:
+        return getTotalPoints(adj_string[::-1])
     return 0
 
-def getPoints(closer):
-        return next(bracket["points"] for bracket in BRACKETS if bracket["close"] == closer)
+def getTotalPoints(string: str):
+    total = 0
+    for i in string:
+        total *= 5
+        total += getPoints(i)
+    return total
+
+def getPoints(opener: str):
+    return next(bracket["points"] for bracket in BRACKETS if bracket["open"] == opener)
 
 lines = openInput()
 
-total = 0
+totals = []
 for line in lines:
-    total += getCorruptPoints(line)
-print(f'{total=}')
+    result = getIncompletePoints(line)
+    if result > 0:
+        totals.append(result)
+
+totals.sort()
+midpoint = (len(totals)+1)/2
+middle_score = totals[int(midpoint)-1]
+print(f'{middle_score=}')
